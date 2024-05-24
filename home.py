d186777a04ed9f57962a50171fba5901aa03c87b
HANS27barron <a01286990@tec.mx>
2024-05-23 22:51:28 -0600

    first commit

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# pip install streamlit
+# streamlit run home.py
+# https://docs.streamlit.io/develop/concepts/architecture/run-your-app
+
+st.title("this is another page")
+
+if st.button("this is a button"):
+    st.write("you clicked the button")
+    st.warning("this is a warning")
